2d80050e8cd0f6c7e70c73913aea429181c1d744
guebebraga <guebe.braga5@gmail.com>
2025-01-09 13:24:52 -0300

    add vocaltech-api-gateway

---
+++ server/vocaltech-api-gateway/src/config/envs.ts
@@ -0,0 +1,30 @@
+// @prefix micro
+import 'dotenv/config';
+import * as Joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+
+  USERS_MICROSERVICE_HOST: string;
+  USERS_MICROSERVICE_PORT: number;
+}
+
+const envSchema = Joi.object({
+  PORT: Joi.number().required(),
+  USERS_MICROSERVICE_HOST: Joi.string().required(),
+  USERS_MICROSERVICE_PORT: Joi.number().required(),
+}).unknown(true);
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars = value as EnvVars;
+
+export const envs = {
+  port: envVars.PORT,
+  usersMicroserviceHost: envVars.USERS_MICROSERVICE_HOST,
+  usersMicroservicePort: envVars.USERS_MICROSERVICE_PORT,
+};
---
+++ server/vocaltech-api-gateway/src/main.ts
@@ -0,0 +1,57 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
+import { ExceptionFilter } from './common/exeptions/rpc-exception.filter';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    bodyParser: true,
+  });
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  app.useGlobalFilters(new ExceptionFilter());
+
+  await app.listen(envs.port);
+
+  logger.log(`Client-Gateway running on port ${envs.port}`);
+}
+bootstrap();
+
+/*
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 3000,
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
+*/
