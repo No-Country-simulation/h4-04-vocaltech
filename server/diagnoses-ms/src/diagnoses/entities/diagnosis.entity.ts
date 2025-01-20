35614ad9a062c196cae7643cdb3474da7d55e09c
guebebraga <guebe.braga5@gmail.com>
2025-01-20 18:09:33 -0300

    add diagnosis entity

--- server/diagnoses-ms/src/diagnoses/entities/diagnosis.entity.ts
+++ server/diagnoses-ms/src/diagnoses/entities/diagnosis.entity.ts
@@ -1,1 +1,24 @@
-export class Diagnosis {}
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type DiagnosisDocument = Diagnosis & Document;
+
+@Schema({ timestamps: true })
+export class Diagnosis {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ type: Date, default: Date.now })
+  createdAt: Date;
+
+  @Prop({ type: String, required: true })
+  leadId: string;
+
+  @Prop({ enum: ['active', 'resolved'], default: 'active' })
+  status: string;
+}
+
+export const DiagnosisSchema = SchemaFactory.createForClass(Diagnosis);
