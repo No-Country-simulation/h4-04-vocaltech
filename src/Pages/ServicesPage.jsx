8357885cd74926d5c596700211adab84633bef16
kelly saldaña <kellysalb@hotmail.com>
2025-02-05 15:55:36 -0500

    organizando detalles

--- src/Pages/ServicesPage.jsx
+++ src/Pages/ServicesPage.jsx
@@ -15,77 +15,79 @@ function ServicesPage() {
       {/* Contenedor principal con columnas bien alineadas */}
       <div className="grid grid-cols-4 gap-8 w-full pb-4">
         {/* Columna 1: Soluciones Adaptadas */}
-        <div className="col-span-1 flex flex-col items-start gap-4 w-[250px]">
+        <div className="col-span-1 flex flex-col items-start gap-9 w-[250px]">
           <div className="flex items-center gap-2 mb-1">
             <img src={infrastructure} alt="infrastructure" className="w-5 h-5" />
             <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Soluciones Adaptadas</h2>
           </div>
 
-          {/* Empresas */}
-          <div className="flex h-[108px] p-2 items-end gap-4 rounded-lg bg-white w-full">
-            <img src={empresa} alt="Empresas" className="h-[100px] w-[100px] rounded-md object-cover -ml-3 mt-4" />
-            <div className="flex flex-col justify-end h-full">
-              <h3 className="text-[13px] font-semibold text-black">Empresas</h3>
-              <p className="text-[13px] font-medium text-[#222] font-manrope text-justify leading-tight">
-                Servicios personalizados para medianas y grandes empresas.
-              </p>
-            </div>
-          </div>
-
           {/* Emprendedores */}
-          <div className="flex h-[108px] p-2 items-end gap-4 rounded-lg bg-white w-full ">
+          <div className="flex h-[108px] p-2 items-end gap-2 rounded-lg bg-white w-full ">
             <img src={emprendedor} alt="Emprendedores" className="h-[100px] w-[100px] rounded-md object-cover -ml-3" />
-            <div className="flex flex-col justify-end h-full">
-              <h3 className="text-[13px] font-semibold text-black">Emprendedores</h3>
-              <p className="text-[13px] font-medium text-[#222] font-manrope text-justify leading-tight">
+            <div className="flex flex-col gap-2 h-[106%]">
+              <h3 className="text-[15px] font-semibold text-black gap-1">Emprendedores</h3>
+              <p className="text-[13px]  text-black font-manrope  leading-tight">
                 Servicios personalizados para startups y pequeños negocios.
               </p>
             </div>
           </div>
+
+          {/* Empresas */}
+          <div className="flex h-[108px] p-2 items-end gap-4 rounded-lg bg-white w-full">
+            <img src={empresa} alt="Empresas" className="h-[100px] w-[100px] rounded-md object-cover -ml-3 mt-4" />
+            <div className="flex flex-col gap-2 h-[110%]">
+              <h3 className="text-[15px] font-semibold text-black">Empresas</h3>
+              <p className="text-[14px] text-black font-manrope leading-tight">
+                Servicios personalizados para medianas y grandes empresas.
+              </p>
+            </div>
+          </div>
         </div>
 
-        {/* Columna 2: Comunicación y Liderazgo */}
-        <div className="col-span-1 w-[250px]">
-          <div className="flex items-center gap-2 mb-1">
-            <img src={Microphone} alt="Microphone" className="w-5 h-5" />
-            <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Comunicación y liderazgo</h2>
-          </div>
-          <ul className="space-y-2 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
-            <li>Comunicación para equipos</li>
-            <li>Mejorar tu presencia ejecutiva</li>
-            <li>Vender tu idea</li>
-            <li>Construir confianza</li>
-          </ul>
-        </div>
+{/* Columna 2: Comunicación y Liderazgo */}
+<div className="col-span-1 w-[250px]">
+  <div className="flex items-center gap-2 mb-[34px]">
+    <img src={Microphone} alt="Microphone" className="w-5 h-5" />
+    <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Comunicación y liderazgo</h2>
+  </div>
+  <ul className="flex flex-col gap-y-3 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
+    <li>Comunicación para equipos</li>
+    <li>Mejorar tu presencia ejecutiva</li>
+    <li>Vender tu idea</li>
+    <li>Construir confianza</li>
+  </ul>
+</div>
 
-        {/* Columna 3: Tecnología y Talento */}
-        <div className="col-span-1 w-[250px]">
-          <div className="flex items-center gap-2 mb-1">
-            <img src={CPU} alt="CPU" className="w-5 h-5" />
-            <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Tecnología y talento</h2>
-          </div>
-          <ul className="space-y-2 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
-            <li>Desarrollo de producto</li>
-            <li>Talento IT validado</li>
-            <li>Optimización de procesos</li>
-            <li>Construcción de MVP</li>
-          </ul>
-        </div>
+{/* Columna 3: Tecnología y Talento */}
+<div className="col-span-1 w-[250px]">
+  <div className="flex items-center gap-2 mb-[34px]">
+    <img src={CPU} alt="CPU" className="w-5 h-5" />
+    <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Tecnología y talento</h2>
+  </div>
+  <ul className="flex flex-col gap-y-3 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
+    <li>Desarrollo de producto</li>
+    <li>Talento IT validado</li>
+    <li>Optimización de procesos</li>
+    <li>Construcción de MVP</li>
+  </ul>
+</div>
 
-        {/* Columna 4: Combinado */}
-        <div className="col-span-1 w-[250px]">
-          <div className="flex items-center gap-2 mb-1">
-            <img src={Round} alt="Round" className="w-5 h-5" />
-            <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Combinado</h2>
-          </div>
-          <ul className="space-y-2 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
-            <li>Comunicar MVP</li>
-            <li>Escalar proyecto</li>
-            <li>Fortalecer liderazgo</li>
-            <li>Estrategias Digitales 360°</li>
-            <li>Gestión Integral de Proyectos</li>
-          </ul>
-        </div>
+{/* Columna 4: Combinado */}
+<div className="col-span-1 w-[250px]">
+  <div className="flex items-center gap-2 mb-[34px]">
+    <img src={Round} alt="Round" className="w-5 h-5" />
+    <h2 className="text-[14px] font-normal text-[#626262] font-manrope">Combinado</h2>
+  </div>
+  <ul className="flex flex-col gap-y-3 text-[#222] text-[15px] font-medium font-manrope text-justify leading-tight">
+    <li>Comunicar MVP</li>
+    <li>Escalar proyecto</li>
+    <li>Fortalecer liderazgo</li>
+    <li>Estrategias Digitales 360°</li>
+    <li>Gestión Integral de Proyectos</li>
+  </ul>
+</div>
+
+
       </div>
 
       {/* Botón de Acción */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,14 +62,20 @@ function Navbar() {
             Ingresar
           </Link>
           <button
-            className="mt-0 w-[90%] max-w-[300px] flex justify-center items-center gap-2 h-[46px] px-4 rounded-[16px] bg-gradient-to-r from-[#2575FC] via-[#052855] to-[#6A11CB] text-[#F8F8F8] font-semibold text-[18px] leading-normal shadow-[inset_2px_4px_4.8px_rgba(64,64,64,0.25),0px_0px_10px_rgba(48,58,197,0.01),0px_0px_9px_rgba(48,58,197,0.11),0px_0px_7px_rgba(48,58,197,0.39),0px_0px_6px_rgba(48,58,197,0.66),0px_0px_3px_rgba(48,58,197,0.76)] bg-[length:200%_200%] hover:animate-gradientMove whitespace-nowrap"
-            onClick={() => {
-              navigate("/formularioinicio");
-              setMenuOpen(false);
-            }}
-          >
-            Obtener Diagnóstico Gratis
-          </button>
+  className="mt-0 w-[90%] max-w-[300px] flex justify-center items-center gap-2 h-[46px] px-4 
+    rounded-[16px] bg-gradient-to-br from-[#2575FC] via-[#052855] to-[#D230E3] text-[#F8F8F8] 
+    font-semibold text-[18px] leading-normal whitespace-nowrap shadow-[inset_2px_4px_4.8px_rgba(64,64,64,0.25),
+    0px_0px_10px_rgba(48,58,197,0.01),0px_0px_9px_rgba(48,58,197,0.11),0px_0px_7px_rgba(48,58,197,0.39),
+    0px_0px_6px_rgba(48,58,197,0.66),0px_0px_3px_rgba(48,58,197,0.76)]
+    hover:animate-gradientMove bg-[size:130%_130%] hover:bg-[position:100%_50%]"
+  onClick={() => {
+    navigate("/formularioinicio");
+    setMenuOpen(false);
+  }}
+>
+  Obtener Diagnóstico Gratis
+</button>
+
         </div>
       </nav>
 
